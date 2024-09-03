c8afb6fbca7b1429951d0bc16f9ecdc4da8428c0
Scarface02 <orszag.a.1999@gmail.com>
2024-09-03 20:43:02 +0200

    pygame screen init

---
+++ main.py
@@ -0,0 +1,19 @@
+import pygame
+from constants import *
+
+def main():
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    print("Starting asteroids!")
+    print(SCREEN_WIDTH)
+    print(SCREEN_HEIGHT)
+    while True:
+        screen.fill("black")
+        pygame.display.flip()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+
+
+if __name__ == "__main__":
+    main()
