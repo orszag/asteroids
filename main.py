b8810a84e8e7b0bec095e8cc7fe3460cf0cb6037
Scarface02 <orszag.a.1999@gmail.com>
2024-09-04 19:30:13 +0200

    FPS set to 60

--- main.py
+++ main.py
@@ -5,14 +5,18 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     print("Starting asteroids!")
-    print(SCREEN_WIDTH)
-    print(SCREEN_HEIGHT)
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+    clock = pygame.time.Clock()
+    dt = 0
+    
     while True:
         screen.fill("black")
         pygame.display.flip()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
